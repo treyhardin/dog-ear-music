06b919af0fabeea153d324a81ae303b85939def9
Trey Hardin <hello@trumancreative.co>
2025-03-08 13:32:48 -0500

    Try weird config hack

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,5 +21,14 @@ export default defineConfig({
     react()
   ],
   output: 'server',
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  vite: {
+    resolve: {
+      // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
+      // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
+      alias: import.meta.env.PROD && {
+        "react-dom/server": "react-dom/server.edge",
+      },
+    },
+  },
 });
